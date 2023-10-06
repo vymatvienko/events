08f276892220336c51bf1827dadb58cdc1c2d818
vymat <vymatvienko@gmail.com>
2023-10-06 16:28:22 +0300

    Игра доработана

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,6 +7,9 @@ export default class GameController {
     this.onCellClick = this.onCellClick.bind(this);
     this.button = document.querySelector('[data-id=board]');
     this.button.addEventListener('click', this.onCellClick);
+
+    this.points = document.querySelector('.points');
+    this.misses = document.querySelector('.misses');
   }
 
   getCell() {
@@ -18,33 +21,34 @@ export default class GameController {
   init() {
     this.gamePlay.drawUi();
 
-    const newsInterval = setInterval(() => {
+    setInterval(() => {
       let newCell = this.getCell();
       while (newCell === this.gamePlay.activeCell) {
         newCell = this.getCell();
       }
       this.gamePlay.redrawPositions(newCell);
       this.gamePlay.activeCell = newCell;
-
-      if (this.miss === 5) {
-        alert('Game over!');
-        clearInterval(newsInterval);
-      }
     }, 1000);
   }
 
   onCellClick(cell) {
     const { target } = cell;
-    const points = document.querySelector('.points');
-    const misses = document.querySelector('.misses');
 
     if (target.classList.contains('goblin')) {
       this.catch += 1;
       target.classList.remove('goblin');
-      points.textContent = (`Очки: ${this.catch}`);
+      this.points.textContent = (`Очки: ${this.catch}`);
     } else {
       this.miss += 1;
-      misses.textContent = (`Промахи: ${this.miss}`);
+      this.misses.textContent = (`Промахи: ${this.miss}`);
+    }
+
+    if (this.miss === 5) {
+      alert(`Игра окончена! Поймано гоблинов: ${this.catch}`);
+      this.catch = 0;
+      this.miss = 0;
+      this.points.textContent = (`Очки: ${this.catch}`);
+      this.misses.textContent = (`Промахи: ${this.miss}`);
     }
   }
 }
